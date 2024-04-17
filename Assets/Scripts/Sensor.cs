b3a14f3e86bb4a714c20e8112486e90aea8e33a8
Szesny32 <bartoszszczesny.studia@gmail.com>
2024-04-17 22:23:04 +0200

    [MGR] Konwersja Euler to Quaternion

--- Assets/Scripts/Sensor.cs
+++ Assets/Scripts/Sensor.cs
@@ -6,50 +6,48 @@ public class Sensor : MonoBehaviour
 {
     
 
-    [SerializeField]
-    private Vector3 accelerometer_unity;
 
     [SerializeField]
-    private Vector3 accelerometer_custom;
-    
-    [SerializeField]
-    private float angle_true;
-    
-    [SerializeField]
-    private Vector3 axis_true;
-
-    [SerializeField]
-    private float angle_1;
-
-    [SerializeField]
-    private Vector3 axis_1;
+    private Quaternion rotation_true;
 
     [SerializeField]
     private Vector3 angles_euler_true;
 
-    
     [SerializeField]
-    private Vector3 angles_euler_1;
+    private Vector3 axis_true;
 
+    [SerializeField]
+    private float angle_true;
 
     [SerializeField]
     private Vector3 transformUp;
 
     [SerializeField]
-    private Vector3 transformUp2;
+    private Vector3 accelerometer_unity;
 
-    [SerializeField]
-    private Vector3 transformUp3;
-
-
-
-    [SerializeField]
-    private Quaternion rotation_true;
+    //[SerializeField]
+    //private Vector3 accelerometer_custom;
 
 
 
-    [SerializeField]
-    private Quaternion rotation_1;
+
+
+    //[SerializeField]
+    //private Vector3 axis_1;
+
+    //[SerializeField]
+    //private float angle_1;
+
+
+
+
+
+    
+
+
+
+    //[SerializeField]
+    //private Quaternion rotation_1;
 
 
 
@@ -64,7 +62,6 @@ public class Sensor : MonoBehaviour
         Debug.DrawRay(transform.position, transform.up, Color.green);
         Debug.DrawRay(transform.position, transform.forward, Color.blue);
         Debug.DrawRay(transform.position, transform.right, Color.red);
-        //Debug.DrawRay(transform.position, accelerometer_custom, Color.yellow);
         Debug.DrawRay(transform.position, axis_true, Color.yellow);
     }
 
@@ -72,41 +69,36 @@ public class Sensor : MonoBehaviour
 
         
         Quaternion inverseRotation = Quaternion.Inverse(transform.rotation);
-        accelerometer_custom.x = 2.0f * (inverseRotation.x * inverseRotation.y - inverseRotation.w * inverseRotation.z);
-        accelerometer_custom.y = -Mathf.Pow(inverseRotation.x, 2) + Mathf.Pow(inverseRotation.y, 2) - Mathf.Pow(inverseRotation.z, 2) + Mathf.Pow(inverseRotation.w, 2);
-        accelerometer_custom.z = 2.0f * (inverseRotation.z * inverseRotation.y + inverseRotation.w * inverseRotation.x);
+        //accelerometer_custom.x = 2.0f * (inverseRotation.x * inverseRotation.y - inverseRotation.w * inverseRotation.z);
+        //accelerometer_custom.y = -Mathf.Pow(inverseRotation.x, 2) + Mathf.Pow(inverseRotation.y, 2) - Mathf.Pow(inverseRotation.z, 2) + Mathf.Pow(inverseRotation.w, 2);
+        //accelerometer_custom.z = 2.0f * (inverseRotation.z * inverseRotation.y + inverseRotation.w * inverseRotation.x);
 
         accelerometer_unity = inverseRotation * Vector3.up;
 
         rotation_true = transform.rotation;
-        rotation_1 = Quaternion.FromToRotation(Vector3.up, accelerometer_custom);
+        //rotation_1 = Quaternion.FromToRotation(Vector3.up, accelerometer_custom);
 
 
         transform.rotation.ToAngleAxis(out angle_true, out axis_true);
-        rotation_1.ToAngleAxis(out angle_1, out axis_1);
+        //rotation_1.ToAngleAxis(out angle_1, out axis_1);
 
     
         transformUp = transform.up;
-        transformUp2 = transform.rotation * Vector3.up;
-        transformUp3 = Quaternion.FromToRotation(Vector3.up, transform.up) * Vector3.up;
+        //transformUp2 = transform.rotation * Vector3.up;
+        //transformUp3 = Quaternion.FromToRotation(Vector3.up, transform.up) * Vector3.up;
 
-        float q1 = axis_true.x * Mathf.Sin((angle_true/ 2.0f) * Mathf.Deg2Rad);
-        Debug.Log(q1);
+        //float q1 = axis_true.x * Mathf.Sin((angle_true/ 2.0f) * Mathf.Deg2Rad);
+        //Debug.Log(q1);
 
 
         //angles_euler_true = transform.eulerAngles;
         //angles_euler_1.x = - Mathf.Atan2(accelerometer_custom.z, Mathf.Sqrt(Mathf.Pow(accelerometer_custom.x, 2) + Mathf.Pow(accelerometer_custom.y, 2))) * Mathf.Rad2Deg;  
        // angles_euler_1.z = Mathf.Atan2 (accelerometer_custom.x, accelerometer_custom.y) * Mathf.Rad2Deg;
 
-
-
-
-
-
     }
     
     public Vector3 GetAccelerometer(){
-        return accelerometer_custom;
+        return accelerometer_unity;
     }
 
 
--- Assets/Scripts/Tester.cs
+++ Assets/Scripts/Tester.cs
@@ -4,23 +4,143 @@ using UnityEngine;
 
 public class Tester : MonoBehaviour
 {
-    public Quaternion quaternion;
-    public Quaternion inverseQuaternion;
-    public Vector3 accelerometer;
-    public Quaternion test;
     
+
+
+    [SerializeField]
+    private Quaternion rotation;
+
+    [SerializeField]
+    private Vector3 eulerAngles;
+
+    [SerializeField]
+    private Vector3 axis;
+
+    [SerializeField]
+    private float angle;
+
+    [SerializeField]
+    private Vector3 transformUp;
+
+    [SerializeField]
+    private Vector3 accelerometer;
+
+
+    [SerializeField]
+    private Quaternion eulerToQuaternion;
+
+    [SerializeField]
+    private Vector3 eulerToAxis;
+
+    [SerializeField]
+    private float eulerToAngle;
+
+
+
+    [SerializeField]
+    private Vector3 axis2;
+
+    [SerializeField]
+    private float angle2;
+
+    [SerializeField]
+    private Quaternion rotation2;
+
+    [SerializeField]
+    private Vector3 euler2;
+
     void Start()
     {
-        quaternion = transform.rotation;
+        
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        transform.rotation = quaternion;
-        inverseQuaternion = Quaternion.Inverse(quaternion);
-        accelerometer = inverseQuaternion * Vector3.up;
-        test = inverseQuaternion * new Quaternion(0,1,0,0);
+        
+        rotation = transform.rotation;
+        eulerAngles = rotation.eulerAngles;
+        transform.rotation.ToAngleAxis(out angle, out axis);
+        transformUp = transform.up;
+
+        Quaternion inverseRotation = Quaternion.Inverse(rotation);
+        accelerometer = inverseRotation * Vector3.up;
+
+        eulerToQuaternion = Quaternion.Euler(eulerAngles);
+        eulerToQuaternion.ToAngleAxis(out eulerToAngle, out eulerToAxis);
+
+        //EulerToAxisAngle(eulerAngles, out angle2, out axis2);
+        rotation2 = Euler(eulerAngles);
+        euler2 = rotation2.eulerAngles;
+
+        //axis2 = transform.up;
+        //angle2 = axisAngleConversion(axis, angle, axis2);
+       
 
     }
+
+    private void EulerToAxisAngle(Vector3 euler, out float angle, out Vector3 axis)
+    {
+
+        Quaternion tmp = Euler(euler);
+        rotation2 =  tmp;//Quaternion.AngleAxis(angle2, axis2);
+
+        ToAngleAxis(tmp, out angle, out axis);
+
+    }
+
+
+    Quaternion AngleAxis(float angle, Vector3 axis)
+    {
+        float halfAngleRad = Mathf.Deg2Rad * angle * 0.5f;
+        float sinHalfAngle = Mathf.Sin(halfAngleRad);
+        float cosHalfAngle = Mathf.Cos(halfAngleRad);
+        
+        float x = axis.x * sinHalfAngle;
+        float y = axis.y * sinHalfAngle;
+        float z = axis.z * sinHalfAngle;
+        float w = cosHalfAngle;
+
+        return new Quaternion(x, y, z, w);
+    }
+
+     public  Quaternion Euler(Vector3 euler) {
+        Quaternion quaternion = new Quaternion();
+        float cx = Mathf.Cos(euler.x * Mathf.Deg2Rad * 0.5f);
+        float cy = Mathf.Cos(euler.y * Mathf.Deg2Rad * 0.5f);
+        float cz = Mathf.Cos(euler.z * Mathf.Deg2Rad * 0.5f);
+        float sx = Mathf.Sin(euler.x * Mathf.Deg2Rad * 0.5f);
+        float sy = Mathf.Sin(euler.y * Mathf.Deg2Rad * 0.5f);
+        float sz = Mathf.Sin(euler.z * Mathf.Deg2Rad * 0.5f);
+
+        quaternion.x = sx * cy * cz + cx * sy * sz;
+        quaternion.y = cx * sy * cz - sx * cy * sz;
+        quaternion.z = cx * cy * sz - sx * sy * cz;
+        quaternion.w = cx * cy * cz + sx * sy * sz;
+
+        return quaternion;
+    }
+
+    public static void ToAngleAxis(Quaternion q, out float angle, out Vector3 axis)
+    {
+        if (Mathf.Abs(q.w) > 1.0f)
+            q.Normalize();
+
+        angle = 2.0f * Mathf.Acos(q.w) * Mathf.Rad2Deg;
+        float s = Mathf.Sqrt(1.0f - q.w * q.w);
+
+        if (s < 0.001f)
+        {
+            // gdy rotacja jest bliska zeru, osia może być dowolna
+            axis = new Vector3(1, 0, 0);
+        }
+        else
+        {
+            axis = new Vector3(q.x / s, q.y / s, q.z / s);
+        }
+    }
+
+    
+
+
 }
+
